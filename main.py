b429195f0fa6aa727afcc294b90f052a31f7c208
andy091954@gmail.com <andy0124@hanmail.net>
2021-08-23 20:16:04 +0900

    현재상황 모니터링 print문 추가

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import gym
 import replay
 import sac
 import torch
+from numpy import round
+import numpy as np
 
 minimum_batch_size = 300
 
@@ -19,7 +21,16 @@ if __name__ == '__main__':
     rpb = replay.replayBuffer(1000000)
     sacModel = sac.SAC(env.observation_space.shape[0], env.action_space.shape[0],hiddenlayer_size,learningRate)
 
+    # Training Loop
+    total_numsteps = 0
+    updates = 0
+
+    
+
     for episode in range(100) :
+        episode_reward = 0
+        episode_steps = 0
+        
         observation = env.reset()
         pastObs = observation
         step = 0
@@ -47,10 +58,35 @@ if __name__ == '__main__':
 
             #할거 다하고 observation을 pastObs에 넣기
             pastObs = observation
-            step = step + 1
+            step += 1
 
+            total_numsteps += 1
+            episode_reward += reward
+
+        print("Episode: {}, total numsteps: {}, episode steps: {}, reward: {}".format(episode, total_numsteps, step, round(np.sum(episode_reward), 2)))
         
-        
+        if episode % 10 == 0 :
+            avg_reward = 0.
+            episodes = 10
+            for _  in range(episodes):
+                state = env.reset()
+                episode_reward = 0
+                done = False
+                while not done:
+
+                    action = sacModel.sample(state)
+                    next_state, reward, done, _ = env.step(action.detach().cpu().numpy()[0])
+                    episode_reward += reward
+
+
+                    state = next_state
+                avg_reward += episode_reward
+            avg_reward /= episodes
+
+
+            print("----------------------------------------")
+            print("Test Episodes: {}, Avg. Reward: {}".format(episodes, round(avg_reward, 2)))
+            print("----------------------------------------")        
         
 
 
